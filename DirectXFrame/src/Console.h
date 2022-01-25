9977bd0bb2244f4635798d33561c4645c38af289
That_Individual Why <2947875090@qq.com>
2022-01-25 15:46:50 +0800

    add error checker

--- DirectXFrame/src/Console.h
+++ DirectXFrame/src/Console.h
@@ -3,9 +3,13 @@
 #include <Windows.h>
 #ifdef _DEBUG
 #define ADD_CONSOLE() \
-AllocConsole(); \
-FILE* ConsoleStream; \
-freopen_s(&ConsoleStream, "CONOUT$", "w", stdout)
+	AllocConsole(); \
+	FILE* ConsoleStream; \
+	freopen_s(&ConsoleStream, "CONOUT$", "w", stdout)
+#define FREE_CONSOLE() \
+	system("pause"); \
+	FreeConsole();\
+	fclose(stdout)
 #else 
 #define ADD_CONSOLE()
 #endif // !DEBUG
